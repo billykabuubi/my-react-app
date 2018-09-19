9c94ff0a5e943aa31272d44c27fc586a3d8ed009
Billy Kabubi <>
2018-09-19 19:55:40 +0100

    third commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ const IndexPage = () => (
     <h1>Learn to design and code React apps</h1>
     <p>Moving than truth, sleep their of into of was moment. Was but so bidding of was with the diesel the not ambushed eminent or and acknowledge slogging throughout.</p>
     <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Link to="/page-2/">Go to page 3</Link>
   </Layout>
 )
 
