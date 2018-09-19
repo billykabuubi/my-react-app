127d7e3117196c4bc2bb4dd977ad4b58656f328a
Billy Kabubi <>
2018-09-19 19:54:02 +0100

    New commit

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+
+const IndexPage = () => (
+  <Layout>
+    <h1>Learn to design and code React apps</h1>
+    <p>Moving than truth, sleep their of into of was moment. Was but so bidding of was with the diesel the not ambushed eminent or and acknowledge slogging throughout.</p>
+    <p>Now go build something great.</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </Layout>
+)
+
+export default IndexPage
